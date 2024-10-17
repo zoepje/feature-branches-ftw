7054affa78922557b06fc5e48f151563e5a94ec9
zoepje <zoe.penner@hva.nl>
2024-10-17 22:28:26 +0200

    edit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # feature-branches-ftw
 It's a demo about using feature branches
+its also my test repo
--- eleventy.config.js
+++ eleventy.config.js
@@ -4,18 +4,20 @@ export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css/");
 
   eleventyConfig.addWatchTarget("src/css/");
+
 	eleventyConfig.addPlugin(pluginWebc, {
     components: "src/_includes/components/*.webc",
   });
+
   return {
     dir: {
       input: 'src',
       includes: '_includes',
       output: '_site',
     },
-    templateFormats: ['md', 'njk', 'html'],
-    markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
+    // templateFormats: ['md', 'njk', 'html'],
+    // markdownTemplateEngine: 'njk',
+    // htmlTemplateEngine: 'njk',
+    // dataTemplateEngine: 'njk',
   }  
 };
